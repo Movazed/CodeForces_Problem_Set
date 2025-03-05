1bd05cde05da98959c2cccd0d7a5d09e03f1baf4
Movazed <arya6v9000@gmail.com>
2025-03-05 23:36:43 +0530

    NEW

--- 1400_119C_basketball_Exercise.cpp
+++ 1400_119C_basketball_Exercise.cpp
@@ -176,22 +176,19 @@ ll getRandomNumber(ll l, ll r) { return uniform_int_distribution<ll>(l,r)(rng); 
 
 
 void solve() {
-    ll n;
-    cin >> n;
-    vector<ll> a(n + 1), b(n + 1), dp(n + 1, 0), dp2(n + 1, 0);
-    for(ll i = 1; i <= n; i++){
-        cin >> a[i];
+    int n;
+    cin>>n;
+    vector<ll> a(n+1),b(n+1),dp1(n+1,0),dp2(n+1,0);
+    for(int i=1;i<=n;i++) cin>>a[i];
+    for(int i=1;i<=n;i++) cin>>b[i];
+ 
+    for(int i=1;i<=n;i++)
+    {
+        dp1[i]=max(dp1[i-1],a[i]+dp2[i-1]);
+        dp2[i]=max(dp2[i-1],b[i]+dp1[i-1]);
     }
-    for(ll i = 1; i <= n; i++){
-        cin >> b[i];
-    }
-
-    for(ll i = 1; i <= n; i++){
-        dp[i] = max(dp[i - 1] , a[i] +dp2[i - 1]);
-        dp2[i] = max(dp[i - 1] , b[i] + dp[i - 1]);
-    }
-
-    cout << max(dp[n], dp2[n]) << nl;
+ 
+    cout<<max(dp1[n],dp2[n]);
 }
 
 int32_t main() {
